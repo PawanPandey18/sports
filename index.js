92daa6025c2e975e171d233d0d14367ccf9349cf
PawanPandey18 <pandeypawan6541@gmail.com>
2024-06-18 10:02:58 +0545

    slider function

--- index.js
+++ index.js
@@ -69,17 +69,61 @@
 //     a +="\n"  
 // }
 // console.log(a);
-let a="";
-for (let i= 0; i<=5; i++) {
+// let a="";
+// for (let i= 0; i<=5; i++) {
     
-    for (let j=0; j<i; j++){
-       a +=" "; 
+//     for (let j=0; j<i; j++){
+//        a +=" "; 
         
-    }
+//     }
 
-    for (let l=0; l<2*(5-i)-1; l++){ 
-        a +="*";      
-     }
-    a +="\n"  
-}
-console.log(a);
+//     for (let l=0; l<2*(5-i)-1; l++){ 
+//         a +="*";      
+//      }
+//     a +="\n"  
+// }
+// console.log(a);
+
+
+// const images=[
+//     {image:'bat.jpeg'}
+//     {image:'boot.jpeg'}
+// ]
+
+// let imagesHtml=document.getElementById('images')
+// function ImagesShow() {
+//     images.map(val,i)=>{
+//         if(i===startindex){
+//             imagesHtml.innerHTML='<div>'
+//         }
+//     }
+    
+// }
+
+        const images = [
+            { image: 'bat.jpeg' },
+            { image: 'boot.jpeg' }
+        ];
+
+        let startIndex = 0;
+        const imagesHtml = document.getElementById('images');
+
+        function showImage() {
+            imagesHtml.innerHTML = `
+                <img src="${images[startIndex].image}" alt="Image" class="w-full h-full object-cover absolute top-0 left-0">
+            `;
+        }
+
+        function nextImage() {
+            startIndex = (startIndex + 1) % images.length;
+            showImage();
+        }
+
+        function prevImage() {
+            startIndex = (startIndex - 1 + images.length) % images.length;
+            showImage();
+        }
+
+        // Initial display
+        showImage();
+
